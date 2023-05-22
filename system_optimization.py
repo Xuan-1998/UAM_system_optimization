cf653bc39e7f5b9028349b5411fd5e5ec425fa06
Xuan-1998 <j503440616@berkeley.edu>
2023-05-21 23:27:14 -0700

    no solutions....

--- system_optimization.py
+++ system_optimization.py
@@ -3,18 +3,18 @@ import pandas as pd
 import numpy as np
 
 # Constants
-T = 1440
+T = int(1440 / 10)
 K = 10
 V = [0, 1, 2]
 
 # Flight time matrix
-tau = [[0, 6.06667, 0], [6.06667, 0, 0], [0, 0, 0]]
+tau = [[0, 6.06667 / 10, 0], [6.06667 / 10, 0, 0], [0, 0, 0]]
 
 # SOC levels drop matrix
-kappa = [[0, 1, 0], [1, 0, 0], [0, 0, 0]]
+kappa = [[0, 0.8 / 10, 0], [0.8 / 10, 0, 0], [0, 0.8 / 10, 0]]
 
-gamma = [3.047619048, 3.047619048, 3.237872699, 3.729470167, 4.404786588, 5.379957014, 6.913363091,
-        9.685271742, 16.30528373, 71.41103553]
+gamma = [3.047619048 / 10, 3.047619048 / 10, 3.237872699 / 10, 3.729470167 / 10, 4.404786588 / 10, 5.379957014 / 10, 6.913363091 / 10,
+        9.685271742 / 10, 16.30528373 / 10, 71.41103553 / 10]
 
 # Example 'schedule.csv' data loading (Please replace with actual file loading if needed)
 # Here, it is simply initialized with zeros.
@@ -27,9 +27,24 @@ DTLA_LAX = data[data['od'] == 'DTLA_LAX']
 # Create the list of lists
 LAX_DTLA = [[1 if i in LAX_DTLA['schedule'].tolist() else 0] for i in range(1440)]
 DTLA_LAX = [[1 if i in DTLA_LAX['schedule'].tolist() else 0] for i in range(1440)]
+
+# Reshape the array
+new_array_LAX_DTLA = np.array(LAX_DTLA).reshape((144, 10))
+new_array_DTLA_LAX = np.array(DTLA_LAX).reshape((144, 10))
+# Convert back to list
+new_array_LAX_DTLA = new_array_LAX_DTLA.tolist()
+new_array_DTLA_LAX = new_array_DTLA_LAX.tolist()
+# Add elements within each cell
+new_array_LAX_DTLA_sum = np.sum(new_array_LAX_DTLA, axis=1)
+new_array_DTLA_LAX_sum = np.sum(new_array_DTLA_LAX, axis=1)
+
+LAX_DTLA = new_array_LAX_DTLA_sum
+DTLA_LAX = new_array_DTLA_LAX_sum
+
 for t in range(T):
-    f_values[t][0][1] = LAX_DTLA[t][0] # get the first (and only) item of the inner list
-    f_values[t][1][0] = DTLA_LAX[t][0] # get the first (and only) item of the inner list
+    f_values[t][0][1] = LAX_DTLA[t] # get the first (and only) item of the inner list
+    f_values[t][1][0] = DTLA_LAX[t] # get the first (and only) item of the inner list
+print(f_values)
 
 
 
@@ -46,6 +61,11 @@ m.setObjective(ni.sum(0, '*', '*'), GRB.MINIMIZE)
 
 # Constraints
 # Please note that because of Gurobi's zero-based indexing, you might need to adjust the indices
+
+
+# for i in V:
+#     m.addConstr(ni[0, i, 10] == 100)
+
 # Here, the indices are not adjusted
 for i in V:
     for k in range(K+1):
@@ -56,7 +76,7 @@ for i in V:
                 else:
                     m.addConstr(ni[t, i, k] == quicksum(uij[t-1-tau[j-1][i-1], j, i, k+kappa[j-1][i-1]] for j in V if j != i and t-1-tau[j-1][i-1] >= 0 and (t-1-tau[j-1][i-1], j, i, k+kappa[j-1][i-1]) in uij) - quicksum(uij[t-1, i, j, k] for j in V if j != i and (t-1, i, j, k) in uij))
 
-# The rest of your constraints
+# The rest of constraints
 for i in V:
     for k in range(K+1):
         if i != 3:
