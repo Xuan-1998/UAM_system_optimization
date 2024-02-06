cb37d684caf8e311ec2fc2eee900d8b9c7e55436
caoalbert <zyfxdtn@gmail.com>
2024-02-06 15:57:28 -0800

    model updates

--- model/queueing_model.py
+++ model/queueing_model.py
@@ -14,7 +14,7 @@ class one_vertiport_system:
         schedule['schedule'] = np.ceil(schedule['schedule'] / 5)
         self.schedule = schedule
 
-        self.vertiport = Vertiport([aircraft(flight_time=2, charge_time=2, soc_change=20) for i in range(fleet_size)], print_log=print_log)
+        self.vertiport = Vertiport([aircraft(aircraft_id="AA{:02d}".format(i), flight_time=2, charge_time=1, soc_change=10) for i in range(fleet_size)], print_log=print_log)
         self.arrival_curve = schedule.groupby('schedule').sum()['num_pax'].reset_index()
 
     def __update__(self, num_flight, num_pax):
@@ -23,11 +23,11 @@ class one_vertiport_system:
 
         num_flight_dispatched, departed_aircraft = self.vertiport.update(num_flight, one_vertiport_system=True)
 
-
         if len(departed_aircraft)*2 >= num_flight:
             pax_served = num_pax.sum()
         else:
-            pax_served = num_pax[:num_flight-len(departed_aircraft)*2].sum()
+            # pax_served = num_pax[:num_flight-len(departed_aircraft)*2].sum()
+            pax_served = num_pax[:len(departed_aircraft)].sum()
         if self.print_log:
             print(pax_served, num_pax)
         return pax_served
@@ -64,8 +64,8 @@ class system:
         schedule = pd.concat([flight_sche, num_pax_flight['num_pax']], axis=1)
         schedule['schedule'] = np.ceil(schedule['schedule'] / 5)
         self.schedule = schedule
-        self.vertiports = [Vertiport([aircraft() for i in range(fleet_size//2)], print_log=print_log), 
-                           Vertiport([aircraft() for i in range(fleet_size-fleet_size//2)], print_log=print_log)]
+        self.vertiports = [Vertiport([aircraft(aircraft_id="AA{:02d}".format(i), flight_time=2, charge_time=1, soc_change=10) for i in range(fleet_size//2)], print_log=print_log), 
+                           Vertiport([aircraft(aircraft_id="AA{:02d}".format(fleet_size-i), flight_time=2, charge_time=1, soc_change=10) for i in range(fleet_size-fleet_size//2)], print_log=print_log)]
 
         self.arrival_curve = schedule.groupby('schedule').sum()['num_pax'].reset_index()
 
@@ -90,6 +90,7 @@ class system:
             pax_served_at_v2 = num_pax[1][:num_flight[1]-num_flight_v2].sum()
         if self.print_log:
             print(pax_served_at_v1, num_pax[0], pax_served_at_v2, num_pax[1])
+
         return pax_served_at_v1+pax_served_at_v2
 
     def logger(self, output_spill=False):
@@ -133,12 +134,30 @@ class system:
 
         
 class aircraft:
-    def __init__(self, intial_soc=80, flight_time=2, charge_time=1, soc_change=10):
+    def __init__(self, aircraft_id, intial_soc=80, flight_time=2, charge_time=1, soc_change=10, 
+                soc_transition_time=np.array([0.0129,0.0133,0.0137,0.0142,0.0147,
+                0.0153,0.0158,0.0166,0.0172,0.018,
+                0.0188,0.0197,0.0207,0.0219,0.0231,
+                0.0245,0.026,0.0278,0.03,0.0323,
+                0.0351,0.0384,0.0423,0.0472,0.0536,
+                0.0617,0.0726,0.0887,0.1136,0.1582,
+                0.2622,0.9278,])*60):
+
+        self.aircraft_id = aircraft_id
         self.soc = intial_soc
         self.delay_timestep = 0
         self.flight_time = flight_time
         self.charge_time = charge_time
         self.soc_change = soc_change
+        self.soc_transition_allowed = []
+        for i in range(len(soc_transition_time)):
+            idx = i
+            cnt = 0
+            while soc_transition_time[i:idx+1].sum() <= 5:
+                cnt += 1
+                idx += 1
+            self.soc_transition_allowed.append(cnt)
+        self.soc_transition_allowed = np.array(self.soc_transition_allowed)
     
     def fly(self):
         self.delay_timestep += self.flight_time
@@ -146,7 +165,8 @@ class aircraft:
     
     def charge(self):
         self.delay_timestep += self.charge_time
-        self.soc += self.soc_change
+        # self.soc += self.soc_change
+        self.soc += self.soc_transition_allowed[int(self.soc / 2.5)] * 2.5
     
     def update(self):
         if self.delay_timestep > 0:
@@ -164,7 +184,7 @@ class Vertiport:
             if (i.soc >= 10) & (i.delay_timestep == 0):
                 idle_aircrafts.append(i)
             if self.print_log:
-                print(f'Idle aircrafts soc {i.soc}, delay {i.delay_timestep}')
+                print(f'Aircraft {i.aircraft_id} soc {i.soc}, delay {i.delay_timestep}')
         return len(idle_aircrafts)
 
     def update(self, num_flight, one_vertiport_system=False):
@@ -175,16 +195,34 @@ class Vertiport:
             print(f"Flight demand: {num_flight}")
 
         departed_aircrafts = []
+        threshold = 5
         for i in self.aircrafts:
-            if (i.soc >= 10) & (i.delay_timestep == 0):
-
-                if num_flight > 0:
+            if num_flight > 0:
+                if (i.soc >= 10) & (i.delay_timestep == 0):
                     i.fly()
                     num_flight -= 1
                     departed_aircrafts.append(i)
-            
-            elif (i.soc == 0) & (i.delay_timestep == 0):
+                    if self.print_log:
+                        print(i.aircraft_id, 'dispatched for flight', i.soc)
+                elif (i.soc < 10) & (i.delay_timestep == 0):
+                    i.charge()
+                    if self.print_log:
+                        print(i.aircraft_id, 'charging becaseu of low soc', i.soc)
+        
+            elif (len(self.aircrafts) > threshold) and (one_vertiport_system == False):
+                if (i.soc >= 50) & (i.delay_timestep == 0):
+                    i.fly()
+                    departed_aircrafts.append(i)
+                    threshold += 1
+                elif i.delay_timestep == 0:
+                    i.charge()
+
+            elif (i.soc <= 70) & (i.delay_timestep == 0):
                 i.charge()
+                if self.print_log:
+                    print(i.aircraft_id, "charging because of idling")
+            
+
 
         if one_vertiport_system == False:
             for i in departed_aircrafts:
